3be032339ed42d8e11bf36b6af33f84860eaa062
Tom BÉNÉ <tom.bene@telecomnancy.eu>
2022-11-30 15:39:11 +0100

    [~] fixed LetExp ast class

--- src/main/java/ast/LetExp.java
+++ src/main/java/ast/LetExp.java
@@ -1,19 +1,17 @@
 package ast;
 
-import java.util.ArrayList;
-
 public class LetExp implements Ast {
 
     public <T> T accept(AstVisitor<T> visitor) {
         return visitor.visit(this);
     }
 
-    public ArrayList<Ast> decls = new ArrayList<Ast>();
-    public ArrayList<Ast> inExprs = new ArrayList<Ast>();
+    public LetDecls letDecls;
+    public LetScope letScope;
 
-    public LetExp(ArrayList<Ast> decls, ArrayList<Ast> inExprs) {
-        this.decls = decls;
-        this.inExprs = inExprs;
+    public LetExp(LetDecls letDecls, LetScope letScope) {
+        this.letDecls = letDecls;
+        this.letScope = letScope;
     }
 
 }
