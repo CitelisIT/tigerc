57d7f1913dec96ffe871e9b6c02c783293c59749
Guillaume RICARD <guillaume.ricard@telecomnancy.eu>
2023-02-26 13:41:31 +0100

    [+] base for codegen visitor

---
+++ src/main/java/codegen/codegenVisitor.java
@@ -0,0 +1,188 @@
+package codegen;
+
+import ast.AstVisitor;
+
+
+public class codegenVisitor implements AstVisitor<String> {
+
+    public String visit(ast.Program program) {
+        // TODO
+    }
+
+    public String visit(ast.Assign assign) {
+        // TODO
+    }
+
+    public String visit(ast.Or or) {
+        // TODO
+    }
+
+    public String visit(ast.And and) {
+        // TODO
+    }
+
+    public String visit(ast.Eq eq) {
+        // TODO
+    }
+
+    public String visit(ast.NotEq notEq) {
+        // TODO
+    }
+
+    public String visit(ast.InfEq infEq) {
+        // TODO
+    }
+
+    public String visit(ast.Inf inf) {
+        // TODO
+    }
+
+    public String visit(ast.SupEq supEq) {
+        // TODO
+    }
+
+    public String visit(ast.Sup sup) {
+        // TODO
+    }
+
+    public String visit(ast.Add add) {
+        // TODO
+    }
+
+    public String visit(ast.Sub sub) {
+        // TODO
+    }
+
+    public String visit(ast.Mult mult) {
+        // TODO
+    }
+
+    public String visit(ast.Div div) {
+        // TODO
+    }
+
+    public String visit(ast.SeqExp seqExp) {
+        // TODO
+    }
+
+    public String visit(ast.Neg neg) {
+        // TODO
+    }
+
+    public String visit(ast.IfThenElse ifThenElse) {
+        // TODO
+    }
+
+    public String visit(ast.WhileExp whileExp) {
+        // TODO
+    }
+
+    public String visit(ast.ForExp forExp) {
+        // TODO
+    }
+
+    public String visit(ast.LetDecls letDecls) {
+        // TODO
+    }
+
+    public String visit(ast.LetScope letScope) {
+        // TODO
+    }
+
+    public String visit(ast.LetExp letExp) {
+        // TODO
+    }
+
+    public String visit(ast.CallExpArgs callExpArgs) {
+        // TODO
+    }
+
+    public String visit(ast.CallExp callExp) {
+        // TODO
+    }
+
+    public String visit(ast.FieldDec fieldDec) {
+        // TODO
+    }
+
+    public String visit(ast.TypeDec typeDec) {
+        // TODO
+    }
+
+    public String visit(ast.TypeDecs typeDecs) {
+        // TODO
+    }
+
+    public String visit(ast.VarDecType varDecType) {
+        // TODO
+    }
+
+    public String visit(ast.VarDecNoType varDecNoType) {
+        // TODO
+    }
+
+    public String visit(ast.FunArgs funArgs) {
+        // TODO
+    }
+
+    public String visit(ast.FunDec funDec) {
+        // TODO
+    }
+
+    public String visit(ast.Id id) {
+        // TODO
+    }
+
+    public String visit(ast.TypeId typeId) {
+        // TODO
+    }
+
+    public String visit(ast.ArrType arrType) {
+        // TODO
+    }
+
+    public String visit(ast.RecType recType) {
+        // TODO
+    }
+
+    public String visit(ast.Subscript subscript) {
+        // TODO
+    }
+
+    public String visit(ast.FieldExp fieldExp) {
+        // TODO
+    }
+
+    public String visit(ast.ArrCreate arrCreate) {
+        // TODO
+    }
+
+    public String visit(ast.FieldCreate fieldCreate) {
+        // TODO
+    }
+
+    public String visit(ast.RecCreateFields recCreateFields) {
+        // TODO
+    }
+
+    public String visit(ast.RecCreate recCreate) {
+        // TODO
+    }
+
+    public String visit(ast.IntLiteral intLiteral) {
+        // TODO
+    }
+
+    public String visit(ast.StringLiteral stringLiteral) {
+        // TODO
+    }
+
+    public String visit(ast.NilLiteral nilLiteral) {
+        // TODO
+    }
+
+    public String visit(ast.BreakLiteral breakLiteral) {
+        // TODO
+    }
+
+}
